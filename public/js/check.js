1774e921b382c577344aab127f2588dd92e9bce3
ibaczewska <izabela.baczewska@gmail.com>
2019-03-04 21:21:42 +0100

    added function for checking bracats

--- public/js/check.js
+++ public/js/check.js
@@ -1,3 +1,37 @@
-const check = (string) => string === 'cattac';
+const containsOpenCats = (inputValue, openingBracats) => {
+  for(let i = 0; i < openingBracats.length; i++){
+    if(inputValue.includes(openingBracats[i])) return true
+  }
+  return false
+}
 
-export { check };
+const check = (inputValue) => {
+  const openingBracats = ['cat', 'CAT', 'cAt'];
+  const closingBracats = ['tac', 'TAC', 'tAc']; 
+    if(containsOpenCats(inputValue, openingBracats)) {
+      let lastBracat;
+      const stack = [];
+      
+      for (let i = 0; i < inputValue.length; i+=3) {
+          let bracat = inputValue.substring(i, i+3);
+          let openBracatExists = openingBracats.includes(bracat);
+          let closeBracatIdExists = closingBracats.includes(bracat);
+  
+          if (openBracatExists) {
+              stack.push(bracat);
+          } else if (closeBracatIdExists) {            
+              if (stack.length === 0) return false;
+  
+              lastBracat = stack.pop();
+              let openId = openingBracats.indexOf(lastBracat)
+              let closeId = closingBracats.indexOf(bracat)
+              if(openId !== closeId) return false;
+          }
+      }
+  
+      if (stack.length !== 0) return false;
+      return true;     
+    }
+    return false
+}
+export {check};
