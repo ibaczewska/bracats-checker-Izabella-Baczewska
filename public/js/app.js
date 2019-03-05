4a2a91f533972d0cba820fcb7637c1054a3d765f
ibaczewska <izabela.baczewska@gmail.com>
2019-03-05 11:56:35 +0100

    added gulpfile and scss

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const formElem = document.getElementById('form');
   const inputElem = document.getElementById('input');
   const resultElem = document.getElementById('result');
+  const wrapperElem = document.getElementById('wrapper');
 
   formElem.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,11 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const result = check(inputElem.value);
 
     resultElem.classList.remove(`${result ? 'in' : ''}correct`);
+    wrapperElem.classList.remove(`${result ? 'in' : ''}correct`);
     resultElem.classList.add(`${!result ? 'in' : ''}correct`);
+    wrapperElem.classList.add(`${!result ? 'in' : ''}correct`);
   });
 
   formElem.addEventListener('input', () => {
     resultElem.classList.remove('correct', 'incorrect');
+    wrapperElem.classList.remove('correct', 'incorrect');
   });
 
 }, false);
